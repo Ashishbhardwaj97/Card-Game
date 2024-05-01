9b6773bed8d36d63f6a871ed42412dc38d15481e
Ashish Kumar <bhardwajashish9717@gmail.com>
2024-05-01 14:00:08 +0530

    Game Logic
    Game logic to check if puzzle matches or not and whether the game is finished or not.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField]
     private Sprite bgImage;
 
+    [SerializeField]
+    private TextMeshProUGUI Turns;
+
+    [SerializeField]
+    private TextMeshProUGUI Matches;
+
     public Sprite[] puzzles;
 
     public List<Sprite> gamePuzzles = new List<Sprite>();
 
     public List<Button> btns = new List<Button>();
+
+    private bool firstGuess, secondGuess;
+
+    private int countGuesses;
+    private int countCorrectGuesses;
+    private int gameGuesses;
+    private int firstGuessIndex, secondGuessIndex;
+
+    private string firstGuessPuzzle, secondGuessPuzzle;
 
     private void Awake()
     {
@@ -24,6 +40,8 @@ public class GameController : MonoBehaviour
         GetButtons();
         AddListeners();
         AddGamePuzzles();
+        Shuffle(gamePuzzles);
+        gameGuesses = gamePuzzles.Count / 2;
     }
 
     void GetButtons()
@@ -65,5 +83,74 @@ public class GameController : MonoBehaviour
     public void PickAPuzzle()
     {
         string currentSelectedButtonName = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+
+        if(!firstGuess)
+        {
+            firstGuess = true;
+            firstGuessIndex = int.Parse(currentSelectedButtonName);
+            firstGuessPuzzle = gamePuzzles[firstGuessIndex].name;
+
+            btns[firstGuessIndex].image.sprite = gamePuzzles[firstGuessIndex];
+        }
+        else if(!secondGuess)
+        {
+            secondGuess = true;
+            secondGuessIndex = int.Parse(currentSelectedButtonName);
+            secondGuessPuzzle = gamePuzzles[secondGuessIndex].name;
+
+            btns[secondGuessIndex].image.sprite = gamePuzzles[secondGuessIndex];
+            countGuesses++;
+            Turns.text = countGuesses.ToString();
+
+            StartCoroutine(CheckIfPuzzleMatch());
+        }
+    }
+
+    IEnumerator CheckIfPuzzleMatch()
+    {
+        yield return new WaitForSeconds(1f);
+
+        if(firstGuessPuzzle == secondGuessPuzzle)
+        {
+            yield return new WaitForSeconds(0.5f);
+
+            btns[firstGuessIndex].interactable = false; 
+            btns[secondGuessIndex].interactable = false;
+            btns[firstGuessIndex].image.color = new Color(0, 0, 0, 0);
+            btns[secondGuessIndex].image.color = new Color(0, 0, 0, 0);
+
+            CheckIfGameIsFinished();
+        }
+        else
+        {
+            yield return new WaitForSeconds(0.5f);
+
+            btns[firstGuessIndex].image.sprite = bgImage;
+            btns[secondGuessIndex].image.sprite = bgImage;
+        }
+        yield return new WaitForSeconds(0.5f);
+        firstGuess = secondGuess = false;
+    }
+
+    void CheckIfGameIsFinished()
+    {
+        countCorrectGuesses++;
+        Matches.text = countCorrectGuesses.ToString();
+
+        if (countCorrectGuesses == gameGuesses)
+        {
+            print("Done");
+        }
+    }
+
+    void Shuffle(List<Sprite> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            Sprite temp = list[i];
+            int randomIndex = Random.Range(0, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
     }
 }
